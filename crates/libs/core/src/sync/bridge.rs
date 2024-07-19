9e2e88c4d596eae8d4eb8e7307197886a36eeb5c
Youyuan Wu <youyuanwu@outlook.com>
2024-07-18 17:11:46 -0700

    add doc

--- crates/libs/core/src/sync/bridge.rs
+++ crates/libs/core/src/sync/bridge.rs
@@ -14,6 +14,20 @@ use windows_core::AsImpl;
 
 use super::bridge_context::BridgeContext;
 
+/// Bridge for SF Begin com api.
+/// This launches the future task on rt, and triggers the callback when
+/// the future completes.
+/// The returned context must be passed up to the Begin api's return.
+/// The End api must use fabric_end_bridge() to retrieve the async result from
+/// the returned context.
+/// Remarks:
+/// This api in a sense is unsafe because the develper needs to ensure the following:
+///     The return type of the future must match the return type of the SF End api.
+/// SF Begin api should use this function to launch the future in the background,
+/// and return the context. When future completes,the result is saved in the context and
+/// callback is triggerd to SF. SF guarantees to call End function to retrieve the
+/// future's result.
+/// See examples in *_bridge.rs runtime mod files for example usage.
 pub fn fabric_begin_bridge<F>(
     rt: &impl Executor,
     callback: Option<&IFabricAsyncOperationCallback>,
@@ -35,6 +49,9 @@ where
     Ok(ctx)
 }
 
+/// Retrieves the future result obtained from fabric_begin_bridge in context.
+/// Must be used in SF End api if its Begin api uses fabric_begin_bridge().
+/// See fabric_begin_bridge for details.
 pub fn fabric_end_bridge<T>(context: Option<&IFabricAsyncOperationContext>) -> T
 where
     T: 'static,

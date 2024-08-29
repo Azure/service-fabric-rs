1d9371b8b265a819c95fa7ef6e153fcbf676d884
Charlie Gettys <Charlie.Gettys@microsoft.com>
2024-08-29 16:33:29 -0700

    Comments

--- crates/samples/no_default_features/src/lib.rs
+++ crates/samples/no_default_features/src/lib.rs
@@ -1,8 +1,15 @@
-//! The known use case for no-default-features is as part of existing apps that already have lifecycle management
-//! in another language
-//! So building a sample app is somewhat more involved
-//! For now, we just check that we can compile mssf-core and mssf-com without any features except for the bundled import libs.
-
+//! If we have an optional feature, we must be able to compile a library or executable without it.
+//!
+//! This simple unmangled function requires no asynchronous calls (which occurs in the real world when integrating new Rust code into existing apps written in C++ or C# that already have lifecycle management logic)
+//!
+//! It also doesn't use the config abstraction (not all apps will want to use config-rs)
+//!
+//! Further, its build.rs provides the path to the import libraries on windows, so it does not need mssf-metadata
+//!
+//! Therefore, none of the crate's 3 default-enabled features is required
+//!
+//! This sample demonstrates it is possible to use the library with default-features = false and ensures that that scenario remains compiling as PRs go into the repository.
+//!
 use mssf_core::runtime::ActivationContext;
 #[no_mangle]
 fn test_fn() {

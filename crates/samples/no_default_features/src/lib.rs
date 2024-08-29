58253a1295110c66a568913a9e8013cc5e4ddae1
Charlie Gettys <Charlie.Gettys@microsoft.com>
2024-08-29 16:26:23 -0700

    Change example

--- crates/samples/no_default_features/src/lib.rs
+++ crates/samples/no_default_features/src/lib.rs
@@ -2,3 +2,13 @@
 //! in another language
 //! So building a sample app is somewhat more involved
 //! For now, we just check that we can compile mssf-core and mssf-com without any features except for the bundled import libs.
+
+use mssf_core::runtime::ActivationContext;
+#[no_mangle]
+fn test_fn()
+{
+    // Make sure we link something
+    //
+    let my_ctx = ActivationContext::create();
+    my_ctx.unwrap();
+}

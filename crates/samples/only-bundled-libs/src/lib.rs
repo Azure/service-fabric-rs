f9e1720e018085cdf230bef876f7082a65fd1b53
Charlie Gettys <Charlie.Gettys@microsoft.com>
2024-08-29 14:50:12 -0700

    Add sample project, start adding feature

---
+++ crates/samples/only-bundled-libs/src/lib.rs
@@ -0,0 +1,4 @@
+/// The known use case for no-default-features is as part of existing apps that already have lifecycle management
+/// in another language
+/// So building a sample app is somewhat more involved
+/// For now, we just check that we can compile mssf-core and mssf-com without any features except for the bundled import libs.

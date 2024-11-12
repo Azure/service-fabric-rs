1616eb7739bea7532dee0c0ddda6505bce2a36d6
Youyuan Wu <youyuanwu@outlook.com>
2024-11-12 13:20:24 -0800

    squash docs

--- crates/libs/core/src/runtime/stateful_bridge.rs
+++ crates/libs/core/src/runtime/stateful_bridge.rs
@@ -470,6 +470,11 @@ where
         let inner = self.inner.clone();
         let r = ReplicaInformation::from(unsafe { replica.as_ref().unwrap() });
         debug!("IFabricPrimaryReplicatorBridge::BeginBuildReplica: {:?}", r);
+        // check the parameter requirements from SF
+        debug_assert_eq!(r.role, ReplicaRole::IdleSecondary);
+        debug_assert_eq!(r.catch_up_capability, -1);
+        debug_assert_eq!(r.current_progress, -1);
+
         let (ctx, token) = BridgeContext3::make(callback);
         ctx.spawn(
             &self.rt,

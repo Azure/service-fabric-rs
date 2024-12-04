4bc2a7c225c6703580d2c7da14425f3f7197481b
Youyuan Wu <youyuanwu@outlook.com>
2024-12-04 13:38:31 -0800

    attempt to upgrade to 0.58

--- crates/libs/com/src/lib.rs
+++ crates/libs/com/src/lib.rs
@@ -17,3 +17,16 @@ pub use mssf_metadata;
 // In linux force to pull in pal lib for linking
 #[cfg(target_os = "linux")]
 extern crate mssf_pal;
+
+// windows core hacks:
+// windows core hack
+// pub mod imp {
+//     pub use external_windows_core::imp::{define_interface, interface_hierarchy, CanInto};
+// }
+// pub use external_windows_core::{
+//     from_raw_borrowed, IUnknown, IUnknownImpl, IUnknown_Vtbl, Interface, Param, Result,
+//     RuntimeName, Type, GUID, HRESULT, PCWSTR,
+// };
+// windows core hack
+use external_windows_core::*;
+extern crate self as windows_core;
--- crates/libs/pal/src/lib.rs
+++ crates/libs/pal/src/lib.rs
@@ -5,3 +5,24 @@
 
 #[cfg(unix)]
 pub mod pal;
+
+// expose all windows core has
+pub use windows_core::*;
+
+// provide other implemenations missing for linux
+// extern crate self as windows_core;
+// This overrides the original on windows as well.
+
+#[repr(transparent)]
+#[derive(Clone, Copy, PartialEq, Eq, Debug)]
+pub struct PCWSTR(pub *const u16);
+
+impl AsRef<PCWSTR> for PCWSTR {
+    fn as_ref(&self) -> &Self {
+        self
+    }
+}
+
+impl windows_core::TypeKind for PCWSTR {
+    type TypeKind = windows_core::CopyType;
+}

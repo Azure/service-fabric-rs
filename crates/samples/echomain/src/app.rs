06b979de615c9074fc80da1e354526ce7c1c944e
Youyuan Wu <youyuanwu@outlook.com>
2025-01-15 14:34:14 -0800

    copyright

--- crates/samples/echomain/src/app.rs
+++ crates/samples/echomain/src/app.rs
@@ -1,3 +1,8 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+
 use mssf_core::WString;
 use tokio::runtime::Handle;
 

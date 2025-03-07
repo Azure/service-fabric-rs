29c2ad224681f75720e0588d786cedac68dc694c
Charlie Gettys <Charlie.Gettys@microsoft.com>
2025-03-06 19:21:13 -0800

    more types

--- crates/libs/core/src/types/common/security_credentials/mod.rs
+++ crates/libs/core/src/types/common/security_credentials/mod.rs
@@ -2,10 +2,18 @@
 // Copyright (c) Microsoft Corporation.  All rights reserved.
 // Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
-
+#![deny(unsafe_op_in_unsafe_fn)]
+#![deny(clippy::undocumented_unsafe_blocks)]
 use mssf_com::FabricClient::IFabricClientSettings2;
+
+mod claims_credentials;
+pub use claims_credentials::*;
 mod fabric_protection_level;
 pub use fabric_protection_level::*;
+mod windows_credentials;
+pub use windows_credentials::*;
+mod x509_credentials;
+pub use x509_credentials::*;
 
 /// Idiomatic FABRIC_SECURITY_CREDENTIALS wrapper
 /// Currently, just a placeholder
---
+++ crates/libs/core/src/types/common/security_credentials/windows_credentials.rs
@@ -0,0 +1,7 @@
+// ------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+// Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
+// ------------------------------------------------------------
+#![deny(unsafe_op_in_unsafe_fn)]
+#![deny(clippy::undocumented_unsafe_blocks)]
+// TODO: implement

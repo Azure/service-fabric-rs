6d9618df9e5557ade1eb30a0ff6c223b7279ff57
Charlie Gettys <Charlie.Gettys@microsoft.com>
2025-03-10 17:30:06 -0700

    windows_Cred_tests

--- crates/libs/core/src/types/common/security_credentials/windows_credentials.rs
+++ crates/libs/core/src/types/common/security_credentials/windows_credentials.rs
@@ -58,4 +58,149 @@ impl FabricSecurityCredentialKind for FabricWindowsCredentials {
 }
 
 #[cfg(test)]
-mod tests {}
+mod test {
+    use mssf_com::FabricClient::IFabricClientSettings2;
+    use mssf_com::FabricTypes::{
+        FABRIC_E_INVALID_CREDENTIALS, FABRIC_PROTECTION_LEVEL_ENCRYPTANDSIGN, FABRIC_PROTECTION_LEVEL_NONE
+    };
+    use std::ptr;
+    use std::sync::{Arc, Mutex};
+
+    use crate::strings::WStringWrap;
+    use crate::types::mockifabricclientsettings::test_utilities::check_array_parameter;
+    use crate::types::mockifabricclientsettings::MockIFabricClientSettings;
+
+    use super::*;
+    const TEST_REMOTE_SPN_1: &str = "TEST_SPN_1";
+    const TEST_REMOTE_IDENTITY_1: &str = "TEST_REMOTE_IDENTITY_1";
+    const TEST_REMOTE_IDENTITY_2: &str = "TEST_REMOTE_IDENTITY_2";
+    fn make_credentials() -> FabricWindowsCredentials {
+        FabricWindowsCredentials {
+            RemoteSpn: WString::from(TEST_REMOTE_SPN_1),
+            RemoteIdentities: vec![WString::from(TEST_REMOTE_IDENTITY_1), WString::from(TEST_REMOTE_IDENTITY_2)],
+            ProtectionLevel: FabricProtectionLevel::EncryptAndSign
+        }
+    }
+
+    fn make_credentials_with_empty_vecs() -> FabricWindowsCredentials {
+        FabricWindowsCredentials {
+            RemoteSpn: WString::new(),
+            RemoteIdentities: vec![],
+            ProtectionLevel: FabricProtectionLevel::None
+        }
+    }
+
+    #[test]
+    fn windows_credentials_nonempty_failure() {
+        let mock = MockIFabricClientSettings::new_all_methods_fail();
+        let creds = make_credentials();
+        let result = creds.apply_inner(mock.into());
+        assert_eq!(
+            result,
+            Err(crate::Error::from(FABRIC_E_INVALID_CREDENTIALS))
+        )
+    }
+
+    #[test]
+    fn windows_credentials_empty_failure() {
+        let mock = IFabricClientSettings2::from(MockIFabricClientSettings::new_all_methods_fail());
+        let creds = make_credentials_with_empty_vecs();
+        let result = creds.apply_inner(mock);
+        assert_eq!(
+            result,
+            Err(crate::Error::from(FABRIC_E_INVALID_CREDENTIALS))
+        )
+    }
+
+    #[test]
+    fn windows_credentials_empty_success() {
+        let call_counter = Arc::new(Mutex::new(0));
+        let call_counter_copy = Arc::clone(&call_counter);
+        let com = MockIFabricClientSettings::new_with_security_credentials_mock(Box::new(
+            move |creds: *const FABRIC_SECURITY_CREDENTIALS| {
+                *call_counter_copy.lock().expect("Not poisoned") += 1;
+                assert!(!creds.is_null() && creds.is_aligned());
+                // SAFETY: test code. non-null and alignment is checked above
+                let creds_copy: FABRIC_SECURITY_CREDENTIALS = unsafe { ptr::read(creds) };
+                assert_eq!(creds_copy.Kind, FABRIC_SECURITY_CREDENTIAL_KIND_WINDOWS);
+
+                let value = creds_copy.Value as *const FABRIC_WINDOWS_CREDENTIALS;
+                assert!(!value.is_null() && value.is_aligned());
+                // SAFETY: test code. non-null and alignment is checked above
+                let value_copy = unsafe { ptr::read(value) };
+                // SAFETY: RemoteIdentityCount and RemoteIdentities go together. Should be valid for dereference.
+                unsafe {
+                    check_array_parameter(
+                        [TEST_REMOTE_IDENTITY_1, TEST_REMOTE_IDENTITY_2],
+                        value_copy.RemoteIdentityCount,
+                        value_copy.RemoteIdentities,
+                    )
+                };
+
+                // SAFETY: test code, must be empty
+                assert!(unsafe {value_copy.RemoteSpn.is_empty()});
+
+                assert_eq!(
+                    value_copy.ProtectionLevel,
+                    FABRIC_PROTECTION_LEVEL_NONE
+                );
+                assert!(value_copy.Reserved.is_null());
+
+                Ok(())
+            },
+        ));
+        // SF might reject this in reality - that's ok, we're making sure our code doesn't have UB
+        let creds = make_credentials_with_empty_vecs();
+        let result = creds.apply_inner(com.into());
+        assert_eq!(result, Ok(()));
+        let actual_call_count = *call_counter.lock().expect("Not poisioned");
+        assert_eq!(actual_call_count, 1)
+    }
+
+    #[test]
+    fn windows_credentials_filled_success() {
+        let call_counter = Arc::new(Mutex::new(0));
+        let call_counter_copy = Arc::clone(&call_counter);
+        let com = MockIFabricClientSettings::new_with_security_credentials_mock(Box::new(
+            move |creds: *const FABRIC_SECURITY_CREDENTIALS| {
+                *call_counter_copy.lock().expect("Not poisoned") += 1;
+                assert!(!creds.is_null() && creds.is_aligned());
+                // SAFETY: test code. non-null and alignment is checked above
+                let creds_copy: FABRIC_SECURITY_CREDENTIALS = unsafe { ptr::read(creds) };
+                assert_eq!(creds_copy.Kind, FABRIC_SECURITY_CREDENTIAL_KIND_WINDOWS);
+
+                let value = creds_copy.Value as *const FABRIC_WINDOWS_CREDENTIALS;
+                assert!(!value.is_null() && value.is_aligned());
+                // SAFETY: test code. non-null and alignment is checked above
+                let value_copy = unsafe { ptr::read(value) };
+                // SAFETY: IssuerThumbprintCount and IssuerThumbprints go together. Should be valid for dereference.
+                unsafe {
+                    check_array_parameter(
+                        [TEST_REMOTE_IDENTITY_1, TEST_REMOTE_IDENTITY_2],
+                        value_copy.RemoteIdentityCount,
+                        value_copy.RemoteIdentities,
+                    )
+                };
+
+                let remote_spn = WStringWrap::from(value_copy.RemoteSpn)
+                    .into_wstring()
+                    .to_string_lossy();
+                assert_eq!(&remote_spn, TEST_REMOTE_SPN_1);
+
+                assert_eq!(
+                    value_copy.ProtectionLevel,
+                    FABRIC_PROTECTION_LEVEL_ENCRYPTANDSIGN
+                );
+                assert!(value_copy.Reserved.is_null());
+
+                Ok(())
+            },
+        ));
+        // SF might reject this in reality - that's ok, we're making sure our code doesn't have UB
+        let creds = make_credentials();
+        let result = creds.apply_inner(com.into());
+        assert_eq!(result, Ok(()));
+        let actual_call_count = *call_counter.lock().expect("Not poisioned");
+        assert_eq!(actual_call_count, 1)
+    }
+}

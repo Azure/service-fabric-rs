689c69c89d3637c909e5f852340661d5483fae82
Youyuan Wu <youyuanwu@outlook.com>
2026-01-12 16:06:35 -0800

    update metadata deps

--- crates/samples/no_default_features/build.rs
+++ crates/samples/no_default_features/build.rs
@@ -3,14 +3,7 @@
 // Licensed under the MIT License (MIT). See License.txt in the repo root for license information.
 // ------------------------------------------------------------
 
-use std::path::Path;
-
 fn main() {
-    if cfg!(windows) {
-        // Add link dir for fabric libs on windows
-        let dir = String::from(r#"build\_deps\fabric_metadata-src\importlibs"#);
-        println!("cargo:rustc-link-search={}", Path::new(&dir).display());
-    }
     let target_env = std::env::var("CARGO_CFG_TARGET_ENV");
     if target_env.as_deref() == Ok("msvc") {
         // Make linking warnings errors on msvc
--- crates/tools/api/src/main.rs
+++ crates/tools/api/src/main.rs
@@ -7,7 +7,7 @@ use std::fs::{self};
 use windows_bindgen::bindgen;
 
 fn main() {
-    let winmd = "./build/_deps/fabric_metadata-src/.windows/winmd/Microsoft.ServiceFabric.winmd";
+    let winmd = "./build/fabric_metadata-src/.windows/winmd/Microsoft.ServiceFabric.winmd";
     // create output dir if not exist
     fs::create_dir_all("crates/libs/com/src/Microsoft/ServiceFabric/").unwrap();
 
@@ -27,55 +27,19 @@ fn main() {
             "windows,skip-root,Windows",
             "--filter",
         ];
-
+        // Note: winmd currently does not contain C free standing functions,
+        // if they are added in the future, we may need to add more filters here.
         let filter_types = vec!["Microsoft.ServiceFabric.FabricTypes"];
         let filter_common = vec![
             "Microsoft.ServiceFabric.FabricCommon", // include fabric types
-            // exclude functions
-            "!Microsoft.ServiceFabric.FabricCommon.FabricDecryptText",
-            "!Microsoft.ServiceFabric.FabricCommon.FabricDecryptValue",
-            "!Microsoft.ServiceFabric.FabricCommon.FabricEncryptText",
-            "!Microsoft.ServiceFabric.FabricCommon.FabricEncryptText2",
-            "!Microsoft.ServiceFabric.FabricCommon.FabricEncryptValue",
-            "!Microsoft.ServiceFabric.FabricCommon.FabricGetLastErrorMessage",
         ];
 
         let filter_runtime = vec![
             "Microsoft.ServiceFabric.FabricRuntime", // include fabric types
-            // exclude functions
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricBeginCreateRuntime",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricBeginGetActivationContext",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricBeginGetCodePackageActivator",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricBeginGetNodeContext",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricCreateKeyValueStoreReplica",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricCreateKeyValueStoreReplica2",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricCreateKeyValueStoreReplica3",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricCreateKeyValueStoreReplica4",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricCreateKeyValueStoreReplica5",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricCreateRuntime",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricEndCreateRuntime",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricEndGetActivationContext",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricEndGetCodePackageActivator",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricEndGetNodeContext",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricGetActivationContext",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricGetCodePackageActivator",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricGetNodeContext",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricLoadEseLocalStoreSettings",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricLoadReplicatorSettings",
-            "!Microsoft.ServiceFabric.FabricRuntime.FabricLoadSecurityCredentials",
         ];
 
         let filter_client = vec![
             "Microsoft.ServiceFabric.FabricClient", // include fabric types
-            // exclude functions
-            "!Microsoft.ServiceFabric.FabricClient.FabricCreateClient",
-            "!Microsoft.ServiceFabric.FabricClient.FabricCreateClient2",
-            "!Microsoft.ServiceFabric.FabricClient.FabricCreateClient3",
-            "!Microsoft.ServiceFabric.FabricClient.FabricCreateLocalClient",
-            "!Microsoft.ServiceFabric.FabricClient.FabricCreateLocalClient2",
-            "!Microsoft.ServiceFabric.FabricClient.FabricCreateLocalClient3",
-            "!Microsoft.ServiceFabric.FabricClient.FabricCreateLocalClient4",
-            "!Microsoft.ServiceFabric.FabricClient.FabricGetDefaultRollingUpgradeMonitoringPolicy",
         ];
 
         bindgen(
